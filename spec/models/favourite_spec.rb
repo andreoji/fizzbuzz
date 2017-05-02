bd7d22f2aee005d0df6ba423cec48490e500798c
andre oji <andrewoji@gmail.com>
2017-05-02 21:07:07 +0100

    fixing test after adding user_id foreign key

--- spec/models/favourite_spec.rb
+++ spec/models/favourite_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Favourite, :type => :model do
-  subject { described_class.new(number: 666) }
+  user = User.create(username: 'jose', password: 'elixir')
+  subject { described_class.new(number: 666, user_id: user.id) }
 
   it 'is not valid without a number' do
-    expect(subject).to be_valid
+    subject.number = nil
+    expect(subject).to_not be_valid
+  end
+  it 'is not valid without a user_id' do
+    subject.user_id = nil
+    expect(subject).to_not be_valid
   end
 end
