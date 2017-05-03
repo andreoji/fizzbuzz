b86ef94a92ea332e7321f1ebd6983bd3c7b8b9f9
andre oji <andrewoji@gmail.com>
2017-05-03 06:39:37 +0100

    some untracked relevant to last commit

---
+++ spec/factories.rb
@@ -0,0 +1,20 @@
+FactoryGirl.define do
+  factory :favourite do
+    number {rand(999)}
+    user
+  end
+
+  factory :user do
+    username 'jose'
+    password 'elixir'
+    factory :user_with_favourites do
+      transient do
+        favourites_count 5
+      end
+      after(:create) do |user, evaluator|
+        create_list(:favourite, evaluator.favourites_count, user: user)
+      end
+    end
+  end
+
+end
