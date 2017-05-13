571220a1c86fb570877abd18571a058c3a14b238
andre oji <andrewoji@gmail.com>
2017-05-13 06:32:59 +0100

    simplified logic in call method

--- app/services/favourites_state.rb
+++ app/services/favourites_state.rb
@@ -1,13 +1,12 @@
 class FavouritesState
   def self.call(params, current_page_numbers, saved_favourites)
     marked_as_favourites = params[:marked_as_favourites]
-    currently_displayed_favourites = saved_favourites & current_page_numbers
     favourites_state = { new_favourites: [], removed_favourites: [] }
-    unless marked_as_favourites.nil?
-      marked = marked_as_favourites.map{ |n| n.to_i }
-      favourites_state[:removed_favourites] = (currently_displayed_favourites - marked)
-      favourites_state[:new_favourites] = (marked - saved_favourites)
-    end
+    return favourites_state if marked_as_favourites.nil?
+    currently_displayed_favourites = saved_favourites & current_page_numbers
+    marked = marked_as_favourites.map{ |n| n.to_i }
+    favourites_state[:removed_favourites] = (currently_displayed_favourites - marked)
+    favourites_state[:new_favourites] = (marked - saved_favourites)
     favourites_state
   end
 end
